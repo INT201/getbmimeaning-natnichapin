24acb67ab04cc03478b2cd58c23d769cbc039774
Natnicha <Natnicha.pin@mail.kmutt.ac.th>
2022-09-02 00:48:42 +0700

    edit1

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,20 @@
 const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+  let BMI = weight / Math.pow((height/100),2)
+    return BMI
 }
 function getBMIMeaning(weight, height) {
-  //code here
+    let BMI = calculateBMI(weight,height)
+    if(BMI>25.0){
+        return "Overweight"
+    }
+    else if (BMI>=18.5  && BMI<=24.9) {
+        return "Normal weight"
+    }
+    else {
+        return "Underweight"
+    }
+
 }
 module.exports = getBMIMeaning
